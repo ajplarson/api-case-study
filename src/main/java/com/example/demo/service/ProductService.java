2c46f8520d8ebed54542b1cfb113954c87aecc63
Andrew Larson <teddward31415@gmail.com>
2021-10-01 17:57:08 -0500

    reformatted the product get so that is more legible but still struggling with how to test

--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -3,13 +3,11 @@ package com.example.demo.service;
 import com.example.demo.utils.JsonParseException;
 import com.example.demo.utils.JsonUtils;
 import java.util.Optional;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.server.ResponseStatusException;
-import reactor.core.publisher.Mono;
 
 import static java.lang.String.format;
 
@@ -22,32 +20,25 @@ public class ProductService {
   private static final String BASE_URL = "https://redsky.target.com";
 
   private final WebClient client = WebClient.create(BASE_URL);
-
-  public Mono<String> getProductDataResponse(String productId) {
+  
+  private String getProductResponseAsString(String productId) {
     String productEndpoint =
-    format("/v3/pdp/tcin/%s?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics&key=candidate#_blank", productId);
-    return client
-        .get()
-        .uri(productEndpoint)
-        .accept(MediaType.APPLICATION_JSON)
-        .exchangeToMono(
-            response -> {
-              if (response.statusCode().equals(HttpStatus.OK)) {
-                return response.bodyToMono(String.class);
-              } else if (response.statusCode().is4xxClientError()) {
-                return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Product Request Failed, 4xx error"));
-              } else {
-                return response.createException().flatMap(Mono::error);
-              }
-            });
+            format("/v3/pdp/tcin/%s?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics&key=candidate#_blank", productId);
+    var responseSpec = client
+            .get()
+            .uri(productEndpoint)
+            .accept(MediaType.APPLICATION_JSON)
+            .retrieve();
+    return responseSpec.
+            bodyToMono(String.class).block();
   }
 
   public Optional<String> getProductNameFromResponse(String productId) {
     try {
-      var responseString = getProductDataResponse(productId).block();
+      var responseString = getProductResponseAsString(productId);
       var productDataJson = jsonUtils.parseStringToJsonObject(responseString);
       return Optional.of(jsonUtils.getNameFromProductJson(productDataJson));
-    } catch (JsonParseException e) {
+    } catch (JsonParseException jsonParseException) {
       return Optional.empty();
     }
   }
